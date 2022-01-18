8c436917413556650fa9cf9627791d5826039072
Priya <97515669+Priya-tec-dev@users.noreply.github.com>
2022-01-18 17:34:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ module.exports = function simple(string) {
 	celsius *= 9/5;
 	celsius += 32;
 	var obs = (celsius).toFixed(1)
-	var message = string+'\xB0F is ' + obs + '\xB0C.';
+	var message = string+'\xB0C is ' + obs + '\xB0F.';
   return message;
 };
