ecc4afc78975679b670bb17b4812a6e7e266998f
Priya <97515669+Priya-tec-dev@users.noreply.github.com>
2022-01-18 16:59:08 +0530

    Add package

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function simple(string) {
+  if (typeof string !== "int") throw new TypeError("Simple wants a number!");
+	var celsius =0
+	var celsius = string;
+	celsius *= 9/5;
+	celsius += 32;
+	var obs = (celsius).toFixed(1)
+	var message = string+'\xB0F is ' + obs + '\xB0C.';
+  return message;
+};
